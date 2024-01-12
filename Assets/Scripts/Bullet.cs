68bd8a27c86b52b83fdd35b4da183ddd0de37242
REofCC <buzz0131@naver.com>
2024-01-12 16:36:02 +0900

    added simple rotate weapon script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damage;
+    public int penetrate;
     // Start is called before the first frame update
-    void Start()
+    public void Init(float damage, int penetrate)
     {
-        
+        this.damage = damage;
+        this.penetrate = penetrate;
     }
 
     // Update is called once per frame
